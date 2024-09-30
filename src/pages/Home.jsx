d1ea429a3cdc7b8a62d9486e3c9c9ff9773b632a
rajatkarmokar2 <rajatkarmokar2@gmail.com>
2024-09-30 20:26:55 +0530

    build bug fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,4 @@
-import React,{ useEffect,useState } from 'react';
-import { Button,Card,CardBody,Image } from '@nextui-org/react';
-import { NavLink } from 'react-router-dom';
-import { projectList } from './../constants/projectList';
-import { skillList } from '../constants/skillList';
-import { motion } from 'framer-motion';
-import Container from '../components/containers/Container';
-import ProjectCard from '../components/cards/ProjectCard';
-import HeroH1 from '../components/headings/HeroH1';
-import SectionH1 from '../components/headings/SectionH1';
+import React from 'react';
 import HomeHero from '../components/sections/HomeHero';
 import HomeProjects from '../components/sections/HomeProjects';
 import HomeSkills from '../components/sections/HomeSkills';
